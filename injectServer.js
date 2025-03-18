ec11de902665f6bb2b042edbf38c45132223bc4a
Stellaris <me@stellaris.com.co.punjabifree>
2025-03-17 22:04:39 -0400

    level select hotfix

--- injectServer.js
+++ injectServer.js
@@ -1,7 +1,8 @@
+window.addEventListener("message", function(event) {
+    console.log(event.data);
+});
+
 let serverframe = document.createElement("iframe");
 serverframe.src = "https://172.104.10.6/Server.html";
 document.children[0].appendChild(serverframe);
 serverframe.contentWindow.postMessage("GET", "*");
-window.addEventListener("message", function(event) {
-    console.log(event.data);
-});
